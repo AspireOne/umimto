9b227e353f191f786449c305808773de39be30ec
Matěj Pešl <matejpesl1@gmail.com>
2021-05-07 12:18:49 +0200

    run on page load

--- src/content-script.js
+++ src/content-script.js
@@ -1,5 +1,26 @@
-const execute = () => {
-	const script = document.createElement("script");
-	script.src = chrome.extension.getURL("src/injection.js");
-	(document.head || documentElement).appendChild(script);
-};
+// Else it may run before the DOM is loaded.
+window.onload = () => {
+	if (document.readyState === 'loading')
+		document.addEventListener('DOMContentLoaded', () => execute());
+	else
+		execute();
+}
+
+function execute() {
+	const css = '';
+	const js = chrome.extension.getURL("src/injection.js");
+
+    const head = document.head || document.getElementsByTagName('head')[0];
+    const style = document.createElement('style');
+    const script = document.createElement("script");
+
+    style.type = 'text/css';
+    script.type = 'text/javascript';
+
+    style.appendChild(document.createTextNode(css));
+    // Else it may run before the DOM is loaded.
+    setTimeout(() => script.src = js, 300);
+	
+	head.appendChild(script);
+	head.appendChild(style);
+}
